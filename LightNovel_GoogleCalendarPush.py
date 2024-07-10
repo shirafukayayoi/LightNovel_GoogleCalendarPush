156733807e295ca5a49fd63e4a1dfdd38a8b039b
shirafukayayoi <ron0614kyuki2@gmail.com>
2024-07-10 18:44:57 +0900

    初期のコードが間違っていたため変更

--- LightNovel_GoogleCalendarPush.py
+++ LightNovel_GoogleCalendarPush.py
@@ -2,9 +2,24 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from google.auth.transport.requests import Request
+import datetime
 import os
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urlencode
+from datetime import datetime, date, timedelta
 
-# 1. 認証情報のロード
+# 指定する年と月
+year = 2024
+month = 7
+
+# 出力する出版社を指定
+target_media = ["電撃文庫", "講談社ラノベ文庫", "HJ文庫", "GA文庫", "ガガガ文庫", "ファンタジア文庫", "MF文庫J"]
+
+# カレンダーidを指定
+calendar_id = ''
+
+# GoogleCalendarの認証情報のロード
 creds = None
 # credentials.json ファイルに保存された認証情報をロードする
 if os.path.exists('token.json'):
@@ -22,31 +37,117 @@ if not creds or not creds.valid:
     with open('token.json', 'w') as token:
         token.write(creds.to_json())
 
-# 2. Google Calendar API の使用
+# Google Calendar API の使用
 service = build('calendar', 'v3', credentials=creds)
 
-# 終日イベントの作成
-event = {
-    'summary': 'Google Calendar APIでの終日テストイベント',
-    'location': '東京',
-    'description': 'PythonでGoogle Calendar APIを使って終日イベントを作成します。',
-    'start': {
-        'date': '2024-07-10',
-        'timeZone': 'Asia/Tokyo',
-    },
-    'end': {
-        'date': '2024-07-14',
-        'timeZone': 'Asia/Tokyo',
-    },
-    'reminders': {
-        'useDefault': False,
-        'overrides': [
-            {'method': 'popup', 'minutes': 30},
-        ],
-    },
+# ベースのurl
+base_url = "https://books.rakuten.co.jp/calendar/001017/monthly/"
+query_params = {
+    "tid": f"{year}-{month:02}-01",  # ここで日付を固定してみましたが、必要に応じて変更してください
+    "p": "{}",
+    "s": "14",
+    "#rclist": ""
 }
 
-# 特定のカレンダーIDを指定してイベントを追加する
-calendar_id = '3913e0f7c529226dbd6b896cfc7e2c4cd82cde7fab054f31a05c2f78250ee5ea@group.calendar.google.com'
-event = service.events().insert(calendarId=calendar_id, body=event).execute()
-print('終日イベントが追加されました。')
+# 日本語の日付を変換する関数
+def convert_japanese_date(japanese_date_str, year):
+
+    if '上旬' in japanese_date_str:
+        return f"{year}-{month}-02"
+
+    # 日本語の日付をパースする
+    try:
+        date_obj = datetime.strptime(japanese_date_str, '%m月 %d日')
+    except ValueError:
+        return ''  # パースできない場合も空文字列を返す
+    
+    # 年を追加して目的の形式に変換する
+    formatted_date = date_obj.replace(year=year).strftime('%Y-%m-%d')
+    return formatted_date
+
+
+def get_events(service, calendar_id, time_min=None, time_max=None):
+    events_result = service.events().list(
+        calendarId=calendar_id,
+        timeMin=time_min,
+        timeMax=time_max,
+        singleEvents=True,
+        orderBy='startTime'
+    ).execute()
+    return events_result.get('items', [])
+
+for page in range(1, 5):
+    query_params["p"] = str(page)
+    url = base_url + "?" + urlencode(query_params)
+    req = requests.get(url)
+
+    if req.status_code == 200:
+        req.encoding = req.apparent_encoding
+        html_soup = BeautifulSoup(req.text, "html.parser")
+        
+        title_list = []
+        date_list = []
+
+        titles = html_soup.find_all(class_="item-title__text")
+        if titles:
+            for title in titles:
+                title_list.append(title.get_text().strip())
+        else:
+            print("指定されたクラスの要素が見つかりません。")
+
+        dates = html_soup.find_all(class_="item-release__date")
+        if dates:
+            for date in dates:
+                date_list.append(date.get_text().strip())
+        else:
+            print("指定された要素は見つかりません。")
+
+        media_items = html_soup.find_all(class_="item-title__media")
+
+        for i, title in enumerate(title_list):
+            if i < len(media_items):
+                media = media_items[i].get_text().strip()
+                formatted_date = convert_japanese_date(date_list[i], year)
+
+                def check_duplicate(service, calendar_id, event_date, title):
+                    time_min = (datetime.strptime(event_date, '%Y-%m-%d') + timedelta(days=-1)).strftime('%Y-%m-%d') + 'T00:00:00Z'
+                    time_max = event_date + 'T00:00:00Z'
+                    events = get_events(service, calendar_id, time_min, time_max)
+                    
+                    for event in events:
+                        if event['summary'] == title and 'date' in event['start']:
+                            return True
+                    return False
+
+                if check_duplicate(service, calendar_id, formatted_date, title):
+                    print(f'{title} のイベントは既にカレンダーに存在します。')
+                    continue
+
+                if any(target in media for target in target_media):
+                    event = {
+                        'summary': title,
+                        'start': {
+                            'date': formatted_date,
+                            'timeZone': 'Asia/Tokyo',
+                        },
+                        'end': {
+                        'date': formatted_date,
+                            'timeZone': 'Asia/Tokyo',
+                        },
+                        'reminders': {
+                            'useDefault': False,
+                            'overrides': [
+                                {'method': 'popup', 'minutes': 30},
+                            ],
+                        },
+                    }
+
+                    event = service.events().insert(calendarId=calendar_id, body=event).execute()
+                    print(f'{title} のイベントが追加されました。')
+
+            else:
+                print(f"インデックス {i} が範囲外になったため終了します。")
+                break
+
+    else:
+        print(f"リクエストが失敗しました。ステータスコード: {req.status_code}")
